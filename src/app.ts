92177864a3835072ac91c413536138ebedcc174c
Icaro Peretti <icaroperetti50@gmail.com>
2021-11-06 14:57:00 -0300

    feat: Using socket io to send information to index.html

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code)
 })
 
-app.listen(4000, () => {
-  console.log('Server is running on port 4000')
-})
+export { serverHttp, io }
